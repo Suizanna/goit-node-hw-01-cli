508005035fe2753d240ecb7db00ba17a1dc794ab
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-09-15 03:27:44 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# goit-node-hw-01-cli
+## Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+### node index.mjs --action list
+
+![list](https://user-images.githubusercontent.com/73129581/133343192-cfc91022-224c-4cb8-9ade-64b68749c43b.png)
+
+## Получаем контакт по id
+
+### node index.mjs --action get --id 5
+
+![get](https://user-images.githubusercontent.com/73129581/133346122-029c04ab-660c-44f5-81d4-f8ce919dccc4.png)
+
+## Добавялем контакт
+
+### node index.mjs --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+![add](https://user-images.githubusercontent.com/73129581/133345844-e8546d5a-04a4-44d7-afe6-cc07101e6d57.png)
+
+## Удаляем контакт
+
+### node index.mjs --action remove --id=3
+
+![remove](https://user-images.githubusercontent.com/73129581/133344829-9c82da76-c0f4-4f52-a622-e41f8c1d987a.png)
---
+++ contacts.js
@@ -0,0 +1,79 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+async function getContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    return console.error(error.message);
+  }
+}
+
+async function listContacts() {
+  try {
+    const contacts = await getContacts();
+    console.table(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await getContacts();
+    const contact = contacts.find(
+      (contact) => contact.id === Number(contactId)
+    );
+    if (!contact) {
+      return null;
+    }
+    console.table(contact);
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await getContacts();
+    const contact = await getContactById(contactId);
+
+    if (!contact) return;
+    console.log("Removing a contact...");
+
+    const newContacts = contacts.filter(
+      (contact) => contact.id !== Number(contactId)
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    console.table(await getContacts());
+    console.log("Success remove");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await getContacts();
+    const newContact = { name, email, phone, id: nanoid() };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.table(newContact);
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
